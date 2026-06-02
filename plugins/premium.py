3dad96a6d062213c92f498ac60a67d83c3674c56
ProObito <ahmedumaid707@gmail.com>
2026-06-03 00:29:12 +0530

    Update premium.py

--- plugins/premium.py
+++ plugins/premium.py
@@ -113,7 +113,8 @@ async def my_plan_command(client: Client, message: Message):
             "Ads: Enabled ❌\n"
             "Premium Features: Locked 🔒\n\n"
             "💡 Unlock premium to download files instantly without shorteners!\n"
-            "Contact admin here to buy - @Its_Wriotheslay"
+            "Contact admin here to buy - @Its_Lozo
+            "
         )
         return
 
