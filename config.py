d827eac6db94371fe42d12f08b66bdbe30fd5673
ProObito <ahmedumaid707@gmail.com>
2026-06-03 20:51:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ API_HASH = os.environ.get("API_HASH", "5fecf44f5dd9f46410d6d835491ae4a3")
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003975701563"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "5585016974"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "6654561076"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
